0dbac151037ed728bea23b485059db7570aa6fbb
Gökçe Merdun <agmmnn@gmail.com>
2023-01-28 14:04:49 +0300

    overflow-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter as FontSans } from "@next/font/google";
 
 import "@/styles/globals.css";
+
 import { cn } from "@/lib/utils";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -21,7 +22,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <html lang="en">
         <body
           className={cn(
-            "min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50",
+            "overflow-hidden min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50",
             fontSans.variable
           )}
         >
