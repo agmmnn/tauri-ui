1c44730cf9a15ccaef6b12f49cca29c663a270b7
Gökçe Merdun <agmmnn@gmail.com>
2023-02-01 00:33:46 +0300

    Add [data-theme="dark"]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ["class", '[data-theme="dark"]'],
   content: ["src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
