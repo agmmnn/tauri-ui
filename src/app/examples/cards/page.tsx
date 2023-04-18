4b2bcddeb0aebc4bb4a62b90c87b94ffbca3b7e6
Gökçe Merdun <agmmnn@gmail.com>
2023-04-18 16:30:45 +0300

    remove 'hidden' attribute from examples

--- src/app/examples/cards/page.tsx
+++ src/app/examples/cards/page.tsx
@@ -36,58 +36,40 @@ function DemoContainer({
 
 export default function CardsPage() {
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/cards-light.png"
-          width={1280}
-          height={1214}
-          alt="Cards"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/cards-dark.png"
-          width={1280}
-          height={1214}
-          alt="Cards"
-          className="hidden dark:block"
-        />
+    <div className=" items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
+      <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
+        <DemoContainer>
+          <DemoCreateAccount />
+        </DemoContainer>
+        <DemoContainer>
+          <DemoPaymentMethod />
+        </DemoContainer>
       </div>
-      <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
-        <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-          <DemoContainer>
-            <DemoCreateAccount />
-          </DemoContainer>
-          <DemoContainer>
-            <DemoPaymentMethod />
-          </DemoContainer>
-        </div>
-        <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-          <DemoContainer>
-            <DemoTeamMembers />
-          </DemoContainer>
-          <DemoContainer>
-            <DemoShareDocument />
-          </DemoContainer>
-          <DemoContainer>
-            <DemoDatePicker />
-          </DemoContainer>
-          <DemoContainer>
-            <DemoNotifications />
-          </DemoContainer>
-        </div>
-        <div className="col-span-2 grid items-start gap-6 lg:col-span-1 lg:grid-cols-2 xl:grid-cols-1">
-          <DemoContainer>
-            <DemoReportAnIssue />
-          </DemoContainer>
-          <DemoContainer>
-            <DemoGithub />
-          </DemoContainer>
-          <DemoContainer>
-            <DemoCookieSettings />
-          </DemoContainer>
-        </div>
+      <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
+        <DemoContainer>
+          <DemoTeamMembers />
+        </DemoContainer>
+        <DemoContainer>
+          <DemoShareDocument />
+        </DemoContainer>
+        <DemoContainer>
+          <DemoDatePicker />
+        </DemoContainer>
+        <DemoContainer>
+          <DemoNotifications />
+        </DemoContainer>
       </div>
-    </>
+      <div className="col-span-2 grid items-start gap-6 lg:col-span-1 lg:grid-cols-2 xl:grid-cols-1">
+        <DemoContainer>
+          <DemoReportAnIssue />
+        </DemoContainer>
+        <DemoContainer>
+          <DemoGithub />
+        </DemoContainer>
+        <DemoContainer>
+          <DemoCookieSettings />
+        </DemoContainer>
+      </div>
+    </div>
   )
 }
--- src/app/examples/layout.tsx
+++ src/app/examples/layout.tsx
@@ -1,22 +1,3 @@
-import { Metadata } from "next"
-import Link from "next/link"
-
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { ExamplesNav } from "@/components/examples-nav"
-import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "@/components/page-header"
-
-import { Menu } from "./music/components/menu"
-
-export const metadata: Metadata = {
-  title: "Examples",
-  description: "Check out some examples app built using the components.",
-}
-
 interface ExamplesLayoutProps {
   children: React.ReactNode
 }
