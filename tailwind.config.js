2264cc0bc971b72f78bdc0f6b583a741b0f87904
Gökçe Merdun <agmmnn@gmail.com>
2023-05-06 18:43:04 +0300

    fix tailwind darkMode rule

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
+  darkMode: ["class"],
   content: ["src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
