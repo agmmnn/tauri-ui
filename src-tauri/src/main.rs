0c191f32cd11d25177a5315407052e7cf7fe266d
Gökçe Merdun <agmmnn@gmail.com>
2023-05-23 00:35:02 +0300

    update tauri 2.0.0-alpha.9

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,6 +13,7 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_window::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
