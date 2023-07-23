ce65e1ac26f6c36ee0c37b0caf4e39ef3ae9149d
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-24 01:17:10 +0500

    improved svelte template

--- templates/sveltekit/vite.config.js
+++ templates/sveltekit/vite.config.js
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
   plugins: [sveltekit()]
 });
