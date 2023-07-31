6672809484f0b45a162c980755b02a25acb4d3fe
Gökçe Merdun <agmmnn@gmail.com>
2023-08-01 01:43:35 +0300

    fix distDir, add favicon (template: sveltekit)

--- templates/sveltekit/vite.config.js
+++ templates/sveltekit/vite.config.js
@@ -1,10 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: 1420,
-    strictPort: true,
+    strictPort: true
   },
   plugins: [sveltekit()]
 });
