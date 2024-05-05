bc332ca74b05ac61294e3e7b0dbf7752e5324967
Developer Marcell <devmarcell@protonmail.com>
2024-05-04 22:04:42 -0300

    fix: classname does not exists in DialogPortal

--- templates/next/src/components/ui/dialog.tsx
+++ templates/next/src/components/ui/dialog.tsx
@@ -11,10 +11,9 @@ const Dialog = DialogPrimitive.Root
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = ({
-  className,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
+  <DialogPrimitive.Portal {...props} />
 )
 DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
